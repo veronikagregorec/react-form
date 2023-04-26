f1e66121673e1acc64d33381700ebeb6a9626b55
veronikagregorec <gregorec.veronika@gmail.com>
2023-04-26 15:58:29 +0200

    Firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: " ",
-  authDomain: " ",
-  projectId: " ",
-  storageBucket: " ",
-  messagingSenderId: " ",
-  appId: " ",
+  apiKey: "AIzaSyB5JC4jsujMb7zAi4bx0mhyEdK37WUJsmA",
+  authDomain: "fir-form-7c88d.firebaseapp.com",
+  projectId: "fir-form-7c88d",
+  storageBucket: "fir-form-7c88d.appspot.com",
+  messagingSenderId: "479853944832",
+  appId: "1:479853944832:web:bfdf116d577c0fe528b8e5",
 };
 
 const app = initializeApp(firebaseConfig);
